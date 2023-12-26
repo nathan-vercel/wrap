800c21ec2bdd9f9c9111626a45134d7e896ee8c6
ryan-verc <148745422+ryan-verc@users.noreply.github.com>
2023-12-26 12:34:56 +0000

    l

--- app/api/webhook/route.tsx
+++ app/api/webhook/route.tsx
@@ -1,9 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server';
- 
+import { ApifyClient } from 'apify-client';
+
 export async function POST(request: any) {
     const body = await request.json(); // 👈
-    console.log(body);
+    const actorRunId = body.eventData.actorRunId;
+    const url = `https://api.apify.com/v2/actor-runs/${actorRunId}/dataset/items?token=${process.env.APIFY_KEY}`
+    const response = await fetch(url);
+    const json = await response.json();
+    const follower_count = json[0].followersCount;
+    console.log(follower_count);
     return NextResponse.json({status: 200})
 }
